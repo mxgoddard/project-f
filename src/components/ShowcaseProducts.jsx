b18f38da501d0a29b3d5bfad9b388d2e553f80e7
mxgoddard <maxolivergoddard@gmail.com>
2020-04-20 16:32:55 +0100

    Tidy up CSS

--- src/components/ShowcaseProducts.jsx
+++ src/components/ShowcaseProducts.jsx
@@ -6,7 +6,7 @@ class ShowcaseProducts extends Component {
     render() {
         return (
             <div className='ShowcaseProductsOuterWrapper'>
-                <h3 className='ShowcaseProductCaption'>Products</h3>
+                <h3 className='ShowcaseProductCaption'>Showcase</h3>
                 <div className='ShowcaseProductsDiv'>
                     <ShowcaseProduct product={ this.state.products[0] } />
                     <ShowcaseProduct product={ this.state.products[1] } />
@@ -20,7 +20,7 @@ class ShowcaseProducts extends Component {
     state = {
         products: [
             { name: 'Blue Glove', description: 'This is a blue glove, duh.', price: '5.00', imageLink: 'assets/category_glove_blue2.jpg' },
-            { name: 'Red Gloe', description: 'This is a red glove, duh.', price: '10.00', imageLink: 'assets/category_glove_red2.jpg' },
+            { name: 'Red Glove', description: 'This is a red glove, duh.', price: '10.00', imageLink: 'assets/category_glove_red2.jpg' },
             { name: 'Orange Glove?', description: `If this is orange, whats the other one?`, price: '15.00', imageLink: 'assets/category_glove_blood_orange2.jpg' },
             { name: 'Definitely Orange Glove', description: 'This is definitely an orange glove.', price: '20.00', imageLink: 'assets/category_glove_orange2.jpg' }
         ]
