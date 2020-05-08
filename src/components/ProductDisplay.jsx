cd9ac03ef854db7035b7871234b6ec1609199d63
mxgoddard <maxolivergoddard@gmail.com>
2020-05-08 19:08:44 +0100

    Redesign mobile product page

--- src/components/ProductDisplay.jsx
+++ src/components/ProductDisplay.jsx
@@ -14,47 +14,35 @@ class ProductDisplay extends Component {
     };
 
     render() {
-
+// <img src={ this.state.imageLink } className='DisplayImage' alt='' />
         if (this.state.loading) return <h1>Loading...</h1>
         return (
             <div className='ProductDisplayOuterWrapper'>
                 <div className='ProductDiv'>
-                    <div className='DisplayImages'>
-                        <img src={ this.state.imageLink } className='DisplayImage' alt='' />
+                    <span className='Category'>Vertex Goalkeeper Gloves</span>
+                    <span className='Category2'>£17.95</span>
+                    <div className='NameOfProduct'>
+                        <span>'The Big Red'</span>
+                    </div>
+                    <div className='TempFiller'>
+                        <div className='DisplayImages'>
+                            <img src={ this.state.imageLink } className='DisplayImage' alt='' />
+                            <img src='../assets/category_glove_red4-front.png' className='DisplayImage' alt='' />
+                        </div>
+                        <div className='AddToCartDiv'>
+                            <p>Add to Cart</p>
+                        </div>
                     </div>
                     <div className='ProductInfo'>
-                        <p className='ProductCategory'>Goalkeeper Gloves</p>
-                        <p className='NameOfProduct'>{ this.state.name } | £{ this.state.price }</p>
-                        <p className='ProductDescription'>{ this.state.description }</p>
-                        <div className='ProductInformation'>
-                            <p>Product Features</p>
-                            <ul className='ProductInformationList'>
-                                { this.state.productInformation.split(',').map(el => {
-                                    return (
-                                        <li>{ el }</li>
-                                    );
-                                })}
-                            </ul>
-                        </div>
-                        <div className='SelectSize'>
-                            <span>Select size >>></span>
-                            <div className='SizeButtons'>
-                                <button type="button">7</button>
-                                <button type="button">8</button>
-                                <button type="button">9</button>
-                            </div>
-                        </div>
-                        <div className='SelectColour'>
-                            <span>Select colour >>></span>
-                            <div className='ColourButtons'>
-                                <button type="button" className='BlueButton'>_</button>
-                                <button type="button" className='RedButton'>_</button>
-                                <button type="button" className='OrangeButton'>_</button>
-                            </div>
-                        </div>
-                        <div className='BuyButtonDiv'>
-                            <a href='#buy-item' className='BuyButton'>Buy now</a>
-                        </div>
+                        <p>{ this.state.description }</p>
+                        <p>Glove features:</p>
+                        <ul>
+                            <li>Roll finger glove cut</li>
+                            <li>Contact latex</li>
+                            <li>Thumb wrap</li>
+                            <li>Punching zone</li>
+                            <li>Neoprene glove body</li>
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -71,7 +59,7 @@ class ProductDisplay extends Component {
                 price: '5.00',
                 description: 'This is the blue glove. Unknown origins however guranteed to stop any ball you need to.',
                 productInformation: 'Flat palm cut,Contact Latex - Best on the market,Thumb Wrap,Full elastic wrist strap,Punching zone,Neoprene glove body',
-                imageLink: '../assets/category_glove_blue2.jpg'
+                imageLink: '../assets/category_glove_blue3.JPG.png'
             },
             {
                 id: 2,
@@ -79,7 +67,7 @@ class ProductDisplay extends Component {
                 price: '10.00',
                 description: 'This is the red glove. Unknown origins however guranteed to stop any ball you need to.',
                 productInformation: 'Flat palm cut,Contact Latex - Best on the market,Thumb Wrap,Full elastic wrist strap,Punching zone,Neoprene glove body',
-                imageLink: '../assets/category_glove_red2.jpg'
+                imageLink: '../assets/category_glove_red4.png'
             },
             {
                 id: 3,
@@ -87,7 +75,7 @@ class ProductDisplay extends Component {
                 price: '15.00',
                 description: 'This might be the orange glove. Unknown origins however guranteed to stop any ball you need to.',
                 productInformation: 'Flat palm cut,Contact Latex - Best on the market,Thumb Wrap,Full elastic wrist strap,Punching zone,Neoprene glove body',
-                imageLink: '../assets/category_glove_blood_orange2.jpg'
+                imageLink: '../assets/category_glove_blood_orange3.JPG.png'
             },
             {
                 id: 4,
@@ -95,7 +83,7 @@ class ProductDisplay extends Component {
                 price: '20.00',
                 description: 'This is definitely the orange glove. Unknown origins however guranteed to stop any ball you need to.',
                 productInformation: 'Flat palm cut,Contact Latex - Best on the market,Thumb Wrap,Full elastic wrist strap,Punching zone,Neoprene glove body',
-                imageLink: '../assets/category_glove_orange2.jpg'
+                imageLink: '../assets/category_glove_orange3.JPG.png'
             }
         ];
 
@@ -116,3 +104,44 @@ class ProductDisplay extends Component {
 }
 
 export default ProductDisplay;
+
+/*
+<div className='DisplayImages'>
+    <img src={ this.state.imageLink } className='DisplayImage' alt='' />
+    <img src='../assets/category_glove_red4-front.png' className='DisplayImage' alt='' />
+</div>
+<div className='ProductInfo'>
+    <p className='ProductCategory'>Goalkeeper Gloves</p>
+    <p className='NameOfProduct'>{ this.state.name } | £{ this.state.price }</p>
+    <p className='ProductDescription'>{ this.state.description }</p>
+    <div className='ProductInformation'>
+        <p>Product Features</p>
+        <ul className='ProductInformationList'>
+            { this.state.productInformation.split(',').map(el => {
+                return (
+                    <li>{ el }</li>
+                );
+            })}
+        </ul>
+    </div>
+    <div className='SelectSize'>
+        <span>Select size >>></span>
+        <div className='SizeButtons'>
+            <button type="button">7</button>
+            <button type="button">8</button>
+            <button type="button">9</button>
+        </div>
+    </div>
+    <div className='SelectColour'>
+        <span>Select colour >>></span>
+        <div className='ColourButtons'>
+            <button type="button" className='BlueButton'>_</button>
+            <button type="button" className='RedButton'>_</button>
+            <button type="button" className='OrangeButton'>_</button>
+        </div>
+    </div>
+    <div className='BuyButtonDiv'>
+        <a href='#buy-item' className='BuyButton'>Buy now</a>
+    </div>
+</div>
+*/
--- src/components/ShowcaseProducts.jsx
+++ src/components/ShowcaseProducts.jsx
@@ -20,7 +20,7 @@ class ShowcaseProducts extends Component {
     state = {
         products: [
             { id: 1, name: 'Blue Glove', description: 'This is a blue glove, duh.', price: '5.00', imageLink: 'assets/category_glove_blue3.JPG.png' },
-            { id: 2, name: 'Red Glove', description: 'This is a red glove, duh.', price: '10.00', imageLink: 'assets/category_glove_red3.JPG.png' },
+            { id: 2, name: 'Red Glove', description: 'This is a red glove, duh.', price: '10.00', imageLink: 'assets/category_glove_red4.png' },
             { id: 3, name: 'Orange Glove?', description: `If this is orange, whats the other one?`, price: '15.00', imageLink: 'assets/category_glove_blood_orange3.JPG.png' },
             { id: 4, name: 'Definitely Orange Glove', description: 'This is definitely an orange glove.', price: '20.00', imageLink: 'assets/category_glove_orange3.JPG.png' }
         ]
